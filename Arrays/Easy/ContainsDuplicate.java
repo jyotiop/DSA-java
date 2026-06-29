e47f08e91a27fc9b20a8271a8ac74b0a4a30fb02
Jyoti kushwaha <kushwahajyoti76881@gmail.com>
2026-06-29 13:12:43 +0530

    Add ContainsDuplicate solution

---
+++ Arrays/Easy/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+java// LeetCode 217 - Contains Duplicate
+// Link: https://leetcode.com/problems/contains-duplicate/
+// Approach: HashSet (track seen elements)
+// Time: O(n) | Space: O(n)
+
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
